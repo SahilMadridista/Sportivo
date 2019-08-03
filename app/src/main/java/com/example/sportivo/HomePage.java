8d58f91d1d24407e27e8680c7f4037c8d12e606c
sahil <sahil91098singh@gmail.com>
2019-08-03 20:52:02 +0530

    Edited

--- app/src/main/java/com/example/sportivo/HomePage.java
+++ app/src/main/java/com/example/sportivo/HomePage.java
@@ -7,12 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
-
 import com.google.android.material.navigation.NavigationView;
 
 public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -86,12 +84,7 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
 
         }
 
-
         return true;
-
-
-
-
 
     }
 
